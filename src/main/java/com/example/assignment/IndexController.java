71bab5b7025ead1f4c16d897434b5d7cbe16053d
KJH <eagle625@naver.com>
2021-09-05 13:40:17 +0900

    업데이트

---
+++ src/main/java/com/example/assignment/IndexController.java
@@ -0,0 +1,10 @@
+package com.example.assignment;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+}
